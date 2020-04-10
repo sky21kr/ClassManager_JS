62ce1a5adb666d335502f28faaf0b222d9b233ff
unknown <cyberesident@naver.com>
2020-04-11 02:01:21 +0900

    add UPDATE

--- add.js
+++ add.js
@@ -1,3 +1,29 @@
-const inp = document.getElementsByName("dayOfTheWeek")
+const inp = document.getElementsByName("dayOfTheWeek"),
+    className = document.querySelector(".jsClassName"),
+    btn = document.querySelector(".jsSubmit");
 
 // 값 저장하기
+
+const classInfo = {
+    name:"",
+    dayOfTheWeek: []
+}
+
+function setInfo() {
+    classInfo.name = className.value
+    inp.forEach(element => {
+        classInfo.dayOfTheWeek.push(element.checked)
+    });
+}
+
+function handleSubmit() {
+    setInfo()
+    opener.postMessage(classInfo,"*")
+    window.close();
+}
+
+function init() {
+    btn.addEventListener("click",handleSubmit)
+}
+
+init()
--- class.js
+++ class.js
@@ -1,19 +1,55 @@
 const managementClass = document.querySelector(".jsManagementClass"),
+    classDiv = document.querySelector(".jsClassDiv"),
     classForm = document.querySelector(".jsClassForm"),
-    addForm = document.querySelector
-
+    toDoInput = document.querySelector(".jsToDoInput")
 
 const classObject = {
     name: "",
-    week: []
+    dayOfTheWeek: [],
+    toDos: []
+}
+
+function paintClass() {
+    const form = document.createElement("form")
+    const div = document.createElement("div")
+    const input = document.createElement("input")
+
+    form.classList.add("jsClassForm")
+    div.classList.add("jsClassName")
+    input.classList.add("jsToDoInput")
+
+    div.innerText = classObject.name
+    form.addEventListener("submit", handleSubmit)
+
+    form.appendChild(div)
+    form.appendChild(input)
+
+    classDiv.appendChild(form)
 }
 
 function addClass() {
     window.open("add.html","_blank","width=500, height=500")
+    window.onmessage = function(info) {
+        classObject.name = info.data.name
+        classObject.dayOfTheWeek = info.data.dayOfTheWeek
+        paintClass()
+    }
+}
+
+function paintToDo() {
+    //ToDo 그리기 하기
+}
+
+function handleSubmit() {
+    event.preventDefault();
+    const currentValue = event.path[0][0].value;
+    paintToDo(currentValue)
+    currentValue.value ="";
 }
 
 function init() {
     managementClass.addEventListener("click", addClass)
+    classForm.addEventListener("submit",handleSubmit)
 }
 
 init();
