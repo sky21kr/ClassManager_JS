7f2279c0290c584d53b2ba34c6da199328f3a5fe
ShinSeungMin <sky2197kr@gmail.com>
2020-05-01 03:53:00 +0900

    수업 요일 표시 기능 업데이트

--- class.js
+++ class.js
@@ -1,238 +1,241 @@
 const addBtn = document.querySelector(".addBtn"),
-    classDiv = document.querySelector(".jsClassDiv"),
-    classForm = document.querySelector(".jsClassForm"),
-    toDoInput = document.querySelector(".jsToDoInput")
+  classDiv = document.querySelector(".jsClassDiv"),
+  classForm = document.querySelector(".jsClassForm"),
+  toDoInput = document.querySelector(".jsToDoInput");
 
 var classObject = {
-    ToDoList: {
-        name: "toDoList",
-        toDos: []
-    }
-}
+  ToDoList: {
+    name: "toDoList",
+    toDos: [],
+  },
+};
 
 function askForDelClass(btn) {
-    const result = confirm("해당 수업을 정말로 삭제하시겠습니까 ?")
-    if(result) {
-        deleteClass(btn);
-    }
+  const result = confirm("해당 수업을 정말로 삭제하시겠습니까 ?");
+  if (result) {
+    deleteClass(btn);
+  }
 }
 
 function deleteClass() {
-    classNameForm = event.path[1]
-    className = classNameForm.id
+  classNameForm = event.path[1];
+  className = classNameForm.id;
 
-    event.path[2].removeChild(classNameForm)
-    delete classObject[className]
-    saveToDos()
+  event.path[2].removeChild(classNameForm);
+  delete classObject[className];
+  saveToDos();
 }
 
 function handleDeleteClass(event) {
-    askForDelClass(event)
+  askForDelClass(event);
 }
 
 function handleClassSetting() {
-    window.open("setting.html",event.path[1].id,"width=500, height=500")
+  window.open("setting.html", event.path[1].id, "width=500, height=500");
 }
 
 function paintClass(name) {
-    const form = document.createElement("form")
-    const div = document.createElement("div")
-    const input = document.createElement("input")
-    const ul = document.createElement("ul")
-    const delBtn = document.createElement("button")
-    const settingBtn = document.createElement("button")
+  const form = document.createElement("form");
+  const div = document.createElement("div");
+  const input = document.createElement("input");
+  const ul = document.createElement("ul");
+  const delBtn = document.createElement("button");
+  const settingBtn = document.createElement("button");
 
-    form.classList.add("jsClassForm")
-    form.id = name
-    div.classList.add("jsClassName")
-    input.classList.add("jsToDoInput")
-    ul.classList.add("jsToDoList")
+  if (classObject[name].dayOfTheWeek[new Date().getDay() - 1] === true) {
+    form.classList.add("jsTodayClass"); // 수업 날짜 확인
+  }
 
-    delBtn.classList.add("delBtn")
-    delBtn.classList.add("classBtn")
-    delBtn.type = "button"
-    delBtn.innerText = "❌"
-    delBtn.addEventListener("click", handleDeleteClass)
+  form.classList.add("jsClassForm");
+  form.id = name;
+  div.classList.add("jsClassName");
+  input.classList.add("jsToDoInput");
+  ul.classList.add("jsToDoList");
 
-    settingBtn.classList.add("settingBtn")
-    settingBtn.classList.add("classBtn")
-    settingBtn.type = "button"
-    settingBtn.innerText = "⚙️"
-    settingBtn.addEventListener("click", handleClassSetting)
+  delBtn.classList.add("delBtn");
+  delBtn.classList.add("classBtn");
+  delBtn.type = "button";
+  delBtn.innerText = "❌";
+  delBtn.addEventListener("click", handleDeleteClass);
 
-    div.innerText = name
-    form.addEventListener("submit", handleSubmit)
+  settingBtn.classList.add("settingBtn");
+  settingBtn.classList.add("classBtn");
+  settingBtn.type = "button";
+  settingBtn.innerText = "⚙️";
+  settingBtn.addEventListener("click", handleClassSetting);
 
-    form.appendChild(div)
-    form.appendChild(input)
-    form.appendChild(ul)
-    form.appendChild(delBtn)
-    form.appendChild(settingBtn)
-    classDiv.appendChild(form)
-    saveToDos()
+  div.innerText = name;
+  form.addEventListener("submit", handleSubmit);
+
+  form.appendChild(div);
+  form.appendChild(input);
+  form.appendChild(ul);
+  form.appendChild(delBtn);
+  form.appendChild(settingBtn);
+  classDiv.appendChild(form);
+  saveToDos();
 }
 
 function addClass() {
-    window.open("add.html","_blank","width=500, height=500")
-    window.onmessage = function(info) {
-        const name = info.data.name
-        const dayOfTheWeek = info.data.dayOfTheWeek
+  window.open("add.html", "_blank", "width=500, height=500");
+  window.onmessage = function (info) {
+    const name = info.data.name;
+    const dayOfTheWeek = info.data.dayOfTheWeek;
 
-        for(className in classObject) {
-            if( className == name ){
-                alert("이미 존재하는 수업입니다")
-                return;
-            }
-        }
-
-        const newClassObject = {
-            [info.data.name]: {
-                name: info.data.name,
-                dayOfTheWeek: info.data.dayOfTheWeek,
-                toDos: []
-            }
-        }
-        classObject = {
-            ...classObject,
-            ...newClassObject
-        }
-
-        paintClass(name,dayOfTheWeek)
-
+    for (className in classObject) {
+      if (className == name) {
+        alert("이미 존재하는 수업입니다");
+        return;
+      }
     }
-    
+
+    const newClassObject = {
+      [info.data.name]: {
+        name: info.data.name,
+        dayOfTheWeek: info.data.dayOfTheWeek,
+        toDos: [],
+      },
+    };
+    classObject = {
+      ...classObject,
+      ...newClassObject,
+    };
+
+    paintClass(name, dayOfTheWeek);
+  };
 }
 
 function deleteToDo(event) {
-    const btn = event.target
-    const li = btn.parentNode
-    const ul = li.parentNode
-    const name = ul.parentNode.id
-    const toDo = li.childNodes[1].innerText
+  const btn = event.target;
+  const li = btn.parentNode;
+  const ul = li.parentNode;
+  const name = ul.parentNode.id;
+  const toDo = li.childNodes[1].innerText;
 
-    ul.removeChild(li);
+  ul.removeChild(li);
 
-    //해당하는 toDo 삭제
-    classObject[name]["toDos"].splice(classObject[name]["toDos"].indexOf(toDo),1)
+  //해당하는 toDo 삭제
+  classObject[name]["toDos"].splice(
+    classObject[name]["toDos"].indexOf(toDo),
+    1
+  );
 
-    saveToDos()
+  saveToDos();
 }
 
 function askForDel(btn) {
-    const result = confirm("삭제하시겠습니까 ?")
+  const result = confirm("삭제하시겠습니까 ?");
 
-    if(result) {
-        deleteToDo(btn);
-    }
+  if (result) {
+    deleteToDo(btn);
+  }
 }
 
 function checkListOverlap(id, list) {
-
-    if( classObject[id]["toDos"].indexOf(list) === -1 ) {
-        return false;
-    } else {
-        return true;
-    }
+  if (classObject[id]["toDos"].indexOf(list) === -1) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
-function changeList(id,beforeContent, afterContent) {
-    event.target.innerHTML = afterContent;
-    classObject[id]["toDos"].splice(classObject[id]["toDos"].indexOf(beforeContent),1,afterContent);
+function changeList(id, beforeContent, afterContent) {
+  event.target.innerHTML = afterContent;
+  classObject[id]["toDos"].splice(
+    classObject[id]["toDos"].indexOf(beforeContent),
+    1,
+    afterContent
+  );
 
-    saveToDos();
+  saveToDos();
 }
 
-function handleChangeList(){
-    const id = event.target.parentNode.parentNode.parentNode.id;
-    const beforeContent = event.target.innerHTML;
-    const afterContent = prompt("바꿀 내용을 입력하세요",event.target.innerHTML);
-    
-    if( afterContent === null ) {
+function handleChangeList() {
+  const id = event.target.parentNode.parentNode.parentNode.id;
+  const beforeContent = event.target.innerHTML;
+  const afterContent = prompt("바꿀 내용을 입력하세요", event.target.innerHTML);
 
-    } else if (!checkListOverlap(id,afterContent)) {
-        changeList(id,beforeContent, afterContent);
-    } else {
-        alert('이미 존재하는 리스트입니다.')
-    }
-    
+  if (afterContent === null) {
+  } else if (!checkListOverlap(id, afterContent)) {
+    changeList(id, beforeContent, afterContent);
+  } else {
+    alert("이미 존재하는 리스트입니다.");
+  }
 }
 
 function paintToDo(path, text) {
+  const liPath = path.querySelector("ul");
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  const checkBtn = document.createElement("button");
 
-    const liPath = path.querySelector("ul")
-    const li = document.createElement("li")
-    const span = document.createElement("span")
-    const checkBtn = document.createElement("button")
+  checkBtn.classList.add("checkBtn");
+  checkBtn.innerText = "✔️";
+  checkBtn.type = "button"; // default가 submit이기 때문에 엔터에 반응하므로 button으로 명시
+  checkBtn.addEventListener("click", askForDel);
 
-    checkBtn.classList.add('checkBtn');
-    checkBtn.innerText = "✔️"
-    checkBtn.type="button" // default가 submit이기 때문에 엔터에 반응하므로 button으로 명시
-    checkBtn.addEventListener("click", askForDel);
+  span.innerText = text;
+  span.addEventListener("dblclick", handleChangeList);
 
-    span.innerText = text;
-    span.addEventListener("dblclick", handleChangeList);
+  li.appendChild(checkBtn);
+  li.appendChild(span);
 
-    li.appendChild(checkBtn)
-    li.appendChild(span)
-    
-    liPath.appendChild(li);
+  liPath.appendChild(li);
 
-    saveToDos()
-
+  saveToDos();
 }
 
 function handleSubmit() {
-    event.preventDefault();
-    const formPath = event.path[0]
-    const liPath = event.path[0][0]
-    const name = formPath.querySelector("div").innerText
-    const currentValue = liPath.value
-    
-    liPath.value = "";
-    
-    if(checkListOverlap(name, currentValue) == true ) {
-        alert('이미 존재하는 리스트입니다.');
-        return;
-    }
+  event.preventDefault();
+  const formPath = event.path[0];
+  const liPath = event.path[0][0];
+  const name = formPath.querySelector("div").innerText;
+  const currentValue = liPath.value;
 
-    classObject[name]["toDos"].push(currentValue)
-    
-    classObject[name] = {
-        ...classObject[name],
-        path: formPath
-    }
+  liPath.value = "";
 
-    paintToDo(formPath, currentValue)
+  if (checkListOverlap(name, currentValue) == true) {
+    alert("이미 존재하는 리스트입니다.");
+    return;
+  }
 
-    liPath.value = "";
+  classObject[name]["toDos"].push(currentValue);
+
+  classObject[name] = {
+    ...classObject[name],
+    path: formPath,
+  };
+
+  paintToDo(formPath, currentValue);
+
+  liPath.value = "";
 }
 
 function saveToDos() {
-    localStorage.setItem("classToDos", JSON.stringify(classObject));
+  localStorage.setItem("classToDos", JSON.stringify(classObject));
 }
 
 function loadData() {
-    const loadedData = localStorage.getItem("classToDos");
-    if (loadedData !== null) {
-        const parsedData = JSON.parse(loadedData);
-        classObject = parsedData
-        for(className in parsedData) {
-            if(className != "ToDoList"){
-                paintClass(className)
-            }
-            parsedData[className]["toDos"].forEach( toDo => {
-                const formPath = document.getElementById(`${className}`)
-                paintToDo(formPath, toDo)
-            })
-        }
+  const loadedData = localStorage.getItem("classToDos");
+  if (loadedData !== null) {
+    const parsedData = JSON.parse(loadedData);
+    classObject = parsedData;
+    for (className in parsedData) {
+      if (className != "ToDoList") {
+        paintClass(className);
+      }
+      parsedData[className]["toDos"].forEach((toDo) => {
+        const formPath = document.getElementById(`${className}`);
+        paintToDo(formPath, toDo);
+      });
     }
+  }
 }
 
-
 function init() {
-    loadData()
-    addBtn.addEventListener("click", addClass)
-    classForm.addEventListener("submit",handleSubmit)
+  loadData();
+  addBtn.addEventListener("click", addClass);
+  classForm.addEventListener("submit", handleSubmit);
 }
 
 init();
--- date.js
+++ date.js
@@ -1,32 +1,35 @@
-const dateContainer = document.querySelector(".jsDate")
+const date = document.querySelector(".jsDate"),
+  clock = document.querySelector(".jsClock");
 
 function getDate() {
-    const week = {
-        0:"일요일",
-        1:"월요일",
-        2:"화요일",
-        3:"수요일",
-        4:"목요일",
-        5:"금요일",
-        6:"토요일"
-    }
+  const week = {
+    0: "일요일",
+    1: "월요일",
+    2: "화요일",
+    3: "수요일",
+    4: "목요일",
+    5: "금요일",
+    6: "토요일",
+  };
 
-    const today = new Date();
-    const year = today.getFullYear();
-    const month = today.getMonth()+1;
-    const date = today.getDate();
-    const day = week[today.getDay()];
-    
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1;
+  const day = today.getDate();
+  const dayOfTheWeek = week[today.getDay()];
+  const hour = today.getHours();
+  const minute = today.getMinutes();
+  const seconds = today.getSeconds();
 
-    const now = document.createElement("h1");
-    now.innerText = `${year}년 ${month}월 ${date}일 ${day}`;
-    dateContainer.appendChild(now);
-    
+  date.innerText = `${year}년 ${month}월 ${day}일 ${dayOfTheWeek}`;
+
+  clock.innerText = `${hour < 10 ? `0${hour}` : hour}:${
+    minute < 10 ? `0${minute}` : minute
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
-
 function init() {
-    getDate();
+  setInterval(getDate, 1000);
 }
 
 init();
